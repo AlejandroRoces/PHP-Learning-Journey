c1e7bb47c4b756bc6bcdff8ed4fbbb8745c7ee6a
alesroces03 <aleroces@gmail.com>
2025-03-20 01:43:06 +0100

    20.03 hello world y primeros ejercicios

---
+++ 01_basics/01.variablesYTiposDeDatos.php
@@ -0,0 +1,7 @@
+<?php
+$nombre = "Juan";       // string
+$edad = 30;             // int
+$precio = 15.99;        // float
+$activo = true;         // bool
+
+echo $nombre . " tiene " . $edad . " años.";

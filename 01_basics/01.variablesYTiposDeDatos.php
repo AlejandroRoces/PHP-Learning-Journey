6728c03d0171392e8d3421ac89bc27ec917c5920
alesroces03 <aleroces@gmail.com>
2025-03-20 09:11:04 +0100

    20.03 ejercicios oop de mike para la tarea

--- 01_basics/01.variablesYTiposDeDatos.php
+++ 01_basics/01.variablesYTiposDeDatos.php
@@ -4,4 +4,7 @@ $edad = 30;             // int
 $precio = 15.99;        // float
 $activo = true;         // bool
 
-echo $nombre . " tiene " . $edad . " años.";
+echo "la variable nombre es de tipo => " . $nombre . " => " . gettype($nombre) . "<br>";
+echo "la variable edad es de tipo => " . $edad . " => " . gettype($edad) . "<br>";
+echo "la variable precio es de tipo => " . $precio . " => " . gettype($precio) . "<br>";
+echo "la variable activo es de tipo => " . $activo . " => " . gettype($activo) . "<br>";
